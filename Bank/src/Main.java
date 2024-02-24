c106661b7797d8011b403b40e5498ac9ecf05151
NIFTISHA <NIFTISHA@Tisha>
2024-02-24 12:12:55 +0530

    initial commit

---
+++ Bank/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer hemant = new Customer(1,"Hemant",5000);
+		hemant.Withdrawl(3000);
+		hemant.Withdrawl(3000);
+	}
+
+}
